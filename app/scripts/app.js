16353e7e28d9d31b76a25e0bb81de8443f20f4e8
suhas1294 <sramesh@qwinixtech.com>
2015-09-03 16:36:24 +0530

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name helloApp
+ * @description
+ * # helloApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('helloApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+        .when('/aboutus', {
+        templateUrl: 'views/aboutus.html',
+        controller: 'AboutusCtrl',
+        controllerAs: 'aboutus'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
